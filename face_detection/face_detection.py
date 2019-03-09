ae5ca6b2395c5c97862d28a1894a547b70255410
ActionVison <wgh19911214@126.com>
2019-03-09 23:51:23 +0800

    [NEW]新增自动爬取图片，测试人脸识别效果

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -1,8 +1,10 @@
 import cv2
+import os
 
 filename = "kobe.bmp"
+file_path = "D://img//2"
 
-def detect(filename):
+def detect(filename,file_name_write):
     # haarcascade_frontalface_default.xml存储在package安装的位置
     face_cascade = cv2.CascadeClassifier("C://Users//934554314//Anaconda3//Lib//site-packages//cv2//data//haarcascade_frontalface_default.xml")
     img = cv2.imread(filename)
@@ -12,10 +14,18 @@ def detect(filename):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    #cv2.namedWindow("Face Detected!")
-    #cv2.imshow("Face Detected!", img)
-    cv2.imwrite("Face.jpg", img)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    cv2.namedWindow("Face Detected!")
+    cv2.imshow("Face Detected!", img)
+    cv2.imwrite(file_path + "//"+"result_"+file_name_write, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-detect(filename)
+
+
+file_list = os.listdir(file_path)
+for cur_file in file_list :
+    print(cur_file)
+    img = cv2.imread(file_path + "//" +cur_file )
+    detect(file_path + "//" +cur_file,cur_file)
+    #print(img.shape)
+
